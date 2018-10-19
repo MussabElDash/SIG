e19f1840028d065b271f8a5d2cd91688940cd26e
Mussab ElDash <mussab14@gmail.com>
2018-10-19 15:12:18 -0500

    added equities insertion sql file

--- java/src/main/java/com/revature/beans/AssetPricing.java
+++ java/src/main/java/com/revature/beans/AssetPricing.java
@@ -9,48 +9,56 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table (name="asset_pricing")
+@Table(name = "asset_pricing")
 public class AssetPricing {
-	
+
 	@Id
-	@Column (name="ticker_symbol")
-	@SequenceGenerator(sequenceName="ticker_seq", name="ticker_seq")
-	@GeneratedValue(generator="ticker_seq", strategy = GenerationType.SEQUENCE)
+	@Column(name = "ticker_symbol")
+	@SequenceGenerator(sequenceName = "ticker_seq", name = "ticker_seq")
+	@GeneratedValue(generator = "ticker_seq", strategy = GenerationType.SEQUENCE)
 	private String tickerSymbol;
-	
-	@Column (name="price")
+
+	@Column(name = "company_name")
+	private String companyName;
+
+	@Column(name = "price")
 	private Double price;
-	
+
 	public String getTickerSymbol() {
 		return tickerSymbol;
 	}
+
 	public void setTickerSymbol(String tickerSymbol) {
 		this.tickerSymbol = tickerSymbol;
 	}
+
 	public Double getPrice() {
 		return price;
 	}
+
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+
 	@Override
 	public String toString() {
 		return "AssetPricing [tickerSymbol=" + tickerSymbol + ", price=" + price + "]";
 	}
-	public AssetPricing(String tickerSymbol, Double price) {
+
+	public AssetPricing(String tickerSymbol, String companyName, Double price) {
 		super();
 		this.tickerSymbol = tickerSymbol;
+		this.companyName = companyName;
 		this.price = price;
 	}
+
 	public AssetPricing(Double price) {
 		super();
 		this.price = price;
 	}
+
 	public AssetPricing() {
 		super();
 	}
-	
-	
-	
 
 }
