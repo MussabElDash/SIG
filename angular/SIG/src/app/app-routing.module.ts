3422fc10e82f081dae4cd43ed9682fa112ac6b53
mc0304 <carter.michael01@gmail.com>
2018-10-22 16:38:59 -0500

    Edits to the Account Summary page

--- angular/SIG/src/app/app-routing.module.ts
+++ angular/SIG/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountsSummaryComponent } from './Components/accounts-summary/accounts-summary.component';
+import { OrdersComponent } from './Components/orders/orders.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: '',
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
