8b1d5c14fe6acac9de1e30bbdd7455f7b31ffe3d
Mussab ElDash <mussab14@gmail.com>
2018-10-24 21:03:47 -0500

    JWT sent to angular
    Signed-off-by: Mussab ElDash <mussab14@gmail.com>

--- angular/SIG/src/app/Services/balance.service.ts
+++ angular/SIG/src/app/Services/balance.service.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class BalanceService {
-  // I need this service to return balance(s) from our DB.
-  constructor(private http: HttpClient) { }
+	// I need this service to return balance(s) from our DB.
+	constructor(private http: HttpClient) { }
 
-  httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
+	httpOptions = {
+		headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+	};
 
-  // this is completely out of wack! I chaneged to Balance after get, and to Balance in second line...
-  getBalance() {
-    return this.http.get<Balance[]>("????????????????");
-  }
+	// this is completely out of wack! I chaneged to Balance after get, and to Balance in second line...
+	getBalance() {
+		// return this.http.get<Balance[]>("????????????????");
+	}
 
 }
--- angular/SIG/src/app/app-routing.module.ts
+++ angular/SIG/src/app/app-routing.module.ts
@@ -1,17 +1,30 @@
-import { AccountdetailComponent } from './accountdetail/accountdetail.component';
-import { LoginComponent } from './login/login.component';
-import { NgModule }             from '@angular/core';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AccountsComponent } from './Components/accounts/accounts.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { LoginComponent } from './Components/login/login.component';
+import { OrdersComponent } from './Components/orders/orders.component';
+import { AccountdetailComponent } from './Components/accountdetail/accountdetail.component';
+
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent},
-  { path: 'accountdetail', component: AccountdetailComponent}
+	{ path: '', component: HomeComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'accounts', component: AccountsComponent },
+	{ path: 'home', component: HomeComponent },
+	{ path: 'accounts-summary', component: AccountsComponent },
+	{ path: 'register', component: RegisterComponent },
+	{ path: 'orders', component: OrdersComponent },
+	{ path: 'account-details', component: AccountdetailComponent },
 ];
 
 @NgModule({
-  exports: [ RouterModule ]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }
