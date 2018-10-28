ee3dbc88320e65e9990c91d86efafcd8b79c3466
Mussab ElDash <mussab14@gmail.com>
2018-10-27 21:59:39 -0500

    modifying the remote ip

--- java/src/test/java/com/revature/test/Authentication/RegisterTest.java
+++ java/src/test/java/com/revature/test/Authentication/RegisterTest.java
@@ -1,6 +1,5 @@
 package com.revature.test.Authentication;
 
-import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 import java.nio.charset.Charset;
@@ -27,40 +26,41 @@ public class RegisterTest {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
 		driver = new ChromeDriver();
 //		driver = new HtmlUnitDriver(BrowserVersion.CHROME);
-		url = "http://52.14.64.242:8085/SIG";
+//		url = "http://52.14.64.242:8085/SIG";
+		url = "localhost:4200";
 	}
 
 	@BeforeTest
 	public void setUp() {
 		driver.get(url);
 		elements = new ArrayList<WebElement>();
-		elements.add(driver.findElement(By.cssSelector("input[formcontrolname=username]")));
-		elements.add(driver.findElement(By.cssSelector("input[formcontrolname=password1]")));
-		elements.add(driver.findElement(By.cssSelector("input[formcontrolname=password2]")));
-		elements.add(driver.findElement(By.cssSelector("input[formcontrolname=fname]")));
-		elements.add(driver.findElement(By.cssSelector("input[formcontrolname=lname]")));
-		elements.add(driver.findElement(By.cssSelector("input[formcontrolname=address]")));
-		elements.add(driver.findElement(By.cssSelector("input[formcontrolname=city]")));
-		elements.add(driver.findElement(By.cssSelector("input[formcontrolname=state]")));
-		elements.add(driver.findElement(By.cssSelector("input[formcontrolname=zip]")));
-		elements.add(driver.findElement(By.cssSelector("input[formcontrolname=ssn]")));
-		elements.add(driver.findElement(By.cssSelector("input[formcontrolname=dob]")));
-		elements.add(driver.findElement(By.cssSelector("input[formcontrolname=phone]")));
+		elements.add(driver.findElement(By.cssSelector("app-register input[formcontrolname=username]")));
+		elements.add(driver.findElement(By.cssSelector("app-register input[formcontrolname=password1]")));
+		elements.add(driver.findElement(By.cssSelector("app-register input[formcontrolname=password2]")));
+		elements.add(driver.findElement(By.cssSelector("app-register input[formcontrolname=fname]")));
+		elements.add(driver.findElement(By.cssSelector("app-register input[formcontrolname=lname]")));
+		elements.add(driver.findElement(By.cssSelector("app-register input[formcontrolname=address]")));
+		elements.add(driver.findElement(By.cssSelector("app-register input[formcontrolname=city]")));
+		elements.add(driver.findElement(By.cssSelector("app-register input[formcontrolname=state]")));
+		elements.add(driver.findElement(By.cssSelector("app-register input[formcontrolname=zip]")));
+		elements.add(driver.findElement(By.cssSelector("app-register input[formcontrolname=ssn]")));
+		elements.add(driver.findElement(By.cssSelector("app-register input[formcontrolname=dob]")));
+		elements.add(driver.findElement(By.cssSelector("app-register input[formcontrolname=phone]")));
 	}
 
-	@Test
+	@Test(priority = 1)
 	public void registerFormAvailable() {
 		WebElement form = driver.findElement(By.cssSelector("app-register form"));
 		assertTrue(form.isDisplayed(), "The Registration Form should be visible");
 	}
 
-	@Test
+	@Test(priority = 2)
 	public void failedRegistrations() {
 		int random = (int) (Math.random() * 10);
 		while (random-- > 0) {
 			List<WebElement> ign = getRandomElems();
 			int i = 0;
-//			elements.get(i).clear();
+			elements.get(i).clear();
 			if (!ign.contains(elements.get(i))) {
 				elements.get(i).sendKeys(randString());
 			}
@@ -120,15 +120,10 @@ public class RegisterTest {
 				elements.get(i).sendKeys("1111111111");
 			}
 			driver.findElement(By.cssSelector("app-register button")).click();
-			assertFalse(driver.findElement(By.cssSelector("app-register button")).isEnabled());
-			if (ign.size() == 0) {
-				assertTrue(waitForElem(By.tagName("app-login")).isDisplayed(),
-						"You should be registered successfuly so you be be redirected to the login page");
-			} else {
-				WebElement button = (new WebDriverWait(driver, 10))
-						.until(ExpectedConditions.elementToBeClickable(By.cssSelector("app-register button")));
-				assertTrue(button.isEnabled(), "The register button should be enabled after unsuccessful register");
-			}
+			WebElement button = (new WebDriverWait(driver, 10))
+					.until(ExpectedConditions.elementToBeClickable(By.cssSelector("app-register button")));
+			assertTrue(button.isEnabled(), "The register button should be enabled after unsuccessful register");
+
 		}
 	}
 
@@ -141,6 +136,9 @@ public class RegisterTest {
 		ArrayList<WebElement> elems = new ArrayList<WebElement>();
 		Random rand = new Random();
 		int ran = rand.nextInt(elements.size());
+		while (ran == 0) {
+			ran = rand.nextInt(elements.size());
+		}
 		while (ran-- > 0) {
 			int index = rand.nextInt(elements.size());
 			while (elems.contains(elements.get(index))) {
